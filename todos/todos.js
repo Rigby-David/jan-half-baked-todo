facbcd90be645bc82fac7f9bdccc329b337de5f3
David <Rigby335@hotmail.com>
2023-01-30 12:56:07 -0800

    insert todo on submit and fetching all todos

--- todos/todos.js
+++ todos/todos.js
@@ -16,24 +16,44 @@ const logoutButton = document.querySelector('#logout');
 const deleteButton = document.querySelector('.delete-button');
 
 // let some todo state (an array)
+let todoState = [];
 
 todoForm.addEventListener('submit', async (e) => {
-    // on submit, 
-    // create a todo in supabase using for data
+    // on submit,
+    e.preventDefault();
+    console.log('clicking');
+    // create a todo in supabase using form data
+    const formData = new FormData(todoForm);
+
+    const todo = formData.get('todo');
+
+    await createTodo(todo);
     // reset the form DOM element
+    todoForm.reset();
     // and display the todos
+    displayTodos();
 });
 
 async function displayTodos() {
     // clear the container (.textContent = '')
+    todosEl.textContent = '';
     // fetch the user's todos from supabase
+    const response = await getTodos();
 
+    const todoState = response.data;
+    console.log('todos', todoState);
     // loop through the user's todos
+    for (let todo of todoState) {
         // for each todo, render a new todo DOM element using your render function
+        const newTodoEl = renderTodo(todo);
         // then add an event listener to each todo
+        newTodoEl.addEventListener('click', () => {
             // on click, update the todo in supabase
             // then (shockingly!) call displayTodos() to refresh the list
-        // append the rendered todo DOM element to the todosEl
+            // append the rendered todo DOM element to the todosEl
+        });
+        todosEl.append(newTodoEl);
+    }
 }
 
 window.addEventListener('load', async () => {
